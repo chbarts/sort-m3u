cc1144b3815d771b35added0a13dcda302a71082
Chris Barts <chbarts@gmail.com>
2025-05-16 23:04:10 -0600

    Initial commit

---
+++ sort-m3u.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $lns = "";
+my @arr;
+
+LINE: while (<>) {
+    chomp;
+    if ($_ =~ /^#EXTM3U/) {
+        print "$_\n";
+        next LINE;
+    }
+
+    if ($_ =~ /^#.+/) {
+        if (length($lns) == 0) {
+            $lns = $_;
+        } else {
+            $lns = $lns . "\t" . $_;
+        }
+
+        next LINE;
+    }
+
+    if (length($lns) == 0) {
+        push @arr, $_;
+    } else {
+        $lns = $lns . "\t" . $_;
+        push @arr, $lns;
+        $lns = "";
+    }
+}
+
+@arr = sort @arr;
+
+for my $i (@arr) {
+    $i =~ s{\t}{\n}g;
+    print "$i\n";
+}
