6b8e11b699d588d3e286c6d7bf1ecd12fe90706d
Chris Barts <chbarts@gmail.com>
2025-08-07 21:54:24 -0600

    Add key option.

--- sort-m3u.pl
+++ sort-m3u.pl
@@ -8,30 +8,35 @@ my $flag = 0;
 my $uniq = 0;
 my $trim = 0;
 my $case = 0;
+my $key = "";
 my @arr;
 
-if ((scalar @ARGV) >= 1) {
+while ((scalar @ARGV) >= 1) {
     if (($ARGV[0] eq "-h") or ($ARGV[0] eq "--help")) {
-        print "usage: sort-m3u [-u] [-t] [-c] [FILES...]\n";
+        print "usage: sort-m3u [-u] [-t] [-c] [-k KEY] [FILES...]\n";
         print "-u means only print one of duplicate lines (uniquify output)\n";
         print "-t means trim the identification portion of the M3U record\n";
         print "-c means sort case-insensitively\n";
+        print "-k KEY means sort by value of key in EXTINF information; otherwise, sort by channel name (portion after comma) (also sort by that if key does not exist in some channel)\n";
+        print "-- means stop processing options and treat everything else as a filename\n";
         exit(0);
-    }
-
-    if ($ARGV[0] eq "-u") {
+    } elsif ($ARGV[0] eq "-u") {
         $uniq = 1;
         shift @ARGV;
-    }
-
-    if ($ARGV[0] eq "-t") {
+    } elsif ($ARGV[0] eq "-t") {
         $trim = 1;
         shift @ARGV;
-    }
-
-    if ($ARGV[0] eq "-c") {
+    } elsif ($ARGV[0] eq "-c") {
         $case = 1;
         shift @ARGV;
+    } elsif ($ARGV[0] eq "-k") {
+        shift @ARGV;
+        $key = shift @ARGV;
+    } elsif ($ARGV[0] eq "--") {
+        shift @ARGV;
+        break;
+    } else {
+        break;
     }
 }
 
@@ -65,16 +70,43 @@ LINE: while (<>) {
     }
 }
 
-sub proc { my $s = shift; $s =~ /^#EXTINF:[^,]+,(.+)/; return $1; };
+sub proc { my $s = shift; $s =~ /^#EXTINF:[^,]+,(.+)/; return $1; }
 # https://perlmaven.com/trim
-sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
+sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s }
 
-if (($trim == 1) and ($case == 1)) {
+sub getkey { my $s = shift;
+             my $k = shift;
+             my %res;
+             $s =~ /^#EXTINF:[-0-9]+\s+(.+)/;
+             my $head = $1;
+             while ($head =~ /([-[:alnum:]]+="[^"]+")\s*(.*)/) {
+                 my $kv = $1;
+                 $head = $2;
+                 $kv =~ /([-[:alnum:]]+)="([^"]+)"/;
+                 $res{$1} = $2;
+             }
+
+             if (exists $res{$k}) {
+                 return $res{$k};
+             } else {
+                 return proc($s);
+             }
+         }
+
+if (($trim == 1) and ($case == 1) and (length($key) > 0)) {
+    @arr = sort { fc(trim(getkey($a, $key))) cmp fc(trim(getkey($b, $key))) } @arr;
+} elsif (($trim == 1) and ($case == 1)) {
     @arr = sort { fc(trim(proc($a))) cmp fc(trim(proc($b))) } @arr;
+} elsif (($trim == 1) and (length($key) > 0)) {
+    @arr = sort { trim(getkey($a, $key)) cmp trim(getkey($b, $key)) } @arr;
+} elsif (($case == 1) and (length($key) > 0)) {
+    @arr = sort { fc(getkey($a, $key)) cmp fc(getkey($b, $key)) } @arr;
 } elsif ($trim == 1) {
     @arr = sort { trim(proc($a)) cmp trim(proc($b)) } @arr;
 } elsif ($case == 1) {
     @arr = sort { fc(proc($a)) cmp fc(proc($b)) } @arr;
+} elsif (length($key) > 0) {
+    @arr = sort { getkey($a, $key) cmp getkey($b, $key) } @arr;
 } else {
     @arr = sort { proc($a) cmp proc($b) } @arr;
 }
