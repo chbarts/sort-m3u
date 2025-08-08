9139c1123d9d3e0010367f8fe03b30285ca91485
Chris Barts <chbarts@gmail.com>
2025-08-07 18:52:19 -0600

    Add options

--- sort-m3u.pl
+++ sort-m3u.pl
@@ -4,7 +4,19 @@ use strict;
 
 my $lns = "";
 my $flag = 0;
+my $uniq = 0;
 my @arr;
+
+if ((scalar @ARGV) >= 1) {
+    if ($ARGV[0] eq "-u") {
+        $uniq = 1;
+        shift @ARGV;
+    } elsif (($ARGV[0] eq "-h") or ($ARGV[0] eq "--help")) {
+        print "usage: sort-m3u [-u] [FILES...]\n";
+        print "-u means only print one of duplicate lines (uniquify output)\n";
+        exit(0);
+    }
+}
 
 LINE: while (<>) {
     chomp;
@@ -38,6 +50,17 @@ LINE: while (<>) {
 
 @arr = sort @arr;
 
-for my $i (@arr) {
-    print "$i\n";
+if ($uniq == 1) {
+    my $lst = "";
+    for my $i (@arr) {
+        if ($i ne $lst) {
+            print "$i\n";
+            $lst = $i;
+        }
+    }
+
+} else {
+    for my $i (@arr) {
+        print "$i\n";
+    }
 }
